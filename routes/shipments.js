d2c6745de544e41e6d763082e3190f89ccc36e1c
John <figurefactory@gmail.com>
2021-03-30 18:53:14 -0400

    added JSON schema and route tests

--- routes/shipments.js
+++ routes/shipments.js
@@ -1,7 +1,9 @@
 "use strict";
-
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schemas/shippingSchema.json");
 const express = require("express");
 const router = new express.Router();
+const {BadRequestError} = require("../expressError.js");
 
 const { shipProduct } = require("../shipItApi");
 
@@ -14,8 +16,16 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
+  const result = jsonschema.validate(req.body, orderSchema);
+  if (!result.valid) {
+    // pass validation errors to error handler
+    //  (the "stack" key is generally the most useful)
+    let errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
+
   return res.json({ shipped: shipId });
 });
 
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,5 +1,4 @@
 "use strict";
-
 const request = require("supertest");
 const app = require("../app");
 
@@ -10,9 +9,20 @@ describe("POST /", function () {
       productId: 1000,
       name: "Test Tester",
       addr: "100 Test St",
-      zip: "12345-6789",
+      zipcode: "12345-6789",
     });
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+
+  test("invalid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 500,
+      name: "Test Tester",
+      addr: 20,
+      zipcode: "1234",
+    });
+
+    expect(resp.body).toEqual({ error: expect.any(Object) }); //check for # of messages
   });
 });
