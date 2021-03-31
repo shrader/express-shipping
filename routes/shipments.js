8a26f765032a61f27230a2dfd9aca03da6a3b9de
John <figurefactory@gmail.com>
2021-03-30 20:26:53 -0400

    added new schema and route for multiple shipping ids, and tests for them

--- routes/shipments.js
+++ routes/shipments.js
@@ -1,6 +1,7 @@
 "use strict";
 const jsonschema = require("jsonschema");
 const orderSchema = require("../schemas/shippingSchema.json");
+const multiShippingSchema = require("../schemas/multiShippingSchema.json");
 const express = require("express");
 const router = new express.Router();
 const {BadRequestError} = require("../expressError.js");
@@ -28,6 +29,23 @@ router.post("/", async function (req, res, next) {
 
   return res.json({ shipped: shipId });
 });
+
+//POST /shipments/multi {productIds, name, addr, zip}
+router.post("/multi", async function(req, res, next){
+  const result = jsonschema.validate(req.body, multiShippingSchema);
+  if (!result.valid) {
+    // pass validation errors to error handler
+    //  (the "stack" key is generally the most useful)
+    let errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+  const {name, addr, zip} = req.body
+  const shipIds = req.body.productIds
+    .map(productId => shipProduct({productId, name, addr, zip}))
+    await Promise.all(shipIds);
+  
+  return res.json({ shipped: shipIds });
+})
 
 
 module.exports = router;
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,8 +1,8 @@
 "use strict";
 const request = require("supertest");
-const app = require("../app");
 const shipItApi = require('../shipItApi');
 shipItApi.shipProduct = jest.fn();
+const app = require("../app");
 
 
 describe("POST /", function () {
@@ -14,25 +14,56 @@ describe("POST /", function () {
       productId: 1000,
       name: "Test Tester",
       addr: "100 Test St",
-      zipcode: "12345-6789",
+      zip: "12345-6789",
     });
 
     expect(resp.body).toEqual({ shipped: 1234 });
     expect(resp.statusCode).toEqual(200);
   });
 
-  xtest("invalid", async function () {
-    shipItApi.shipProduct.mockReturnValue({ receipt: { shipId: 2345 } });
+  test("invalid", async function () {
+    shipItApi.shipProduct.mockReturnValue( 2345 );
     
     const resp = await request(app).post("/shipments").send({
       productId: 500,
       name: "Test Tester",
       addr: 20,
-      zipcode: "1234",
+      zip: "1234",
     });
 
-    expect(resp.body.error).toEqual({ message: expect.any(Array) }); //check for # of messages
+    expect(resp.body.error).toEqual({ message: expect.any(Array), status: 400 }); //check for # of messages
     expect(resp.body.error.message.length).toEqual(3);
-    expect(resp.statusCode).toEqual(400);
   });
 });
+
+describe("POST /multi", function () {
+  test("valid", async function () {
+    shipItApi.shipProduct.mockReturnValue(1234);
+    // console.log(shipItApi.shipProduct());
+    
+    const resp = await request(app).post("/shipments/multi").send({
+      productIds: [1000, 1500],
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.body).toEqual({ shipped: [1234, 1234] });
+    expect(resp.statusCode).toEqual(200);
+  });
+
+  test("invalid", async function () {
+    shipItApi.shipProduct.mockReturnValue( 2345 );
+    
+    const resp = await request(app).post("/shipments/multi").send({
+      productIds: [500,600],
+      name: "Test Tester",
+      addr: 20,
+      zip: "1234",
+    });
+
+    expect(resp.body.error).toEqual({ message: expect.any(Array), status: 400 }); 
+    expect(resp.body.error.message.length).toEqual(4);
+  });
+});
+
